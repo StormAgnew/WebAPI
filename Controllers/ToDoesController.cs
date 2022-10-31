42a882ba4098852c979c4f37ba5f32380c1c8dd7
StormAgnew <leeshank96@gmail.com>
2022-10-31 08:52:08 -0500

    Add project files.

---
+++ Controllers/ToDoesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Controllers
+{
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDosController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDosController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Resources
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDo>>> GetToDos()
+        {
+            if (_context.ToDos == null)
+            {
+                return NotFound();
+            }
+
+            var todos = await _context.ToDos.Include("Category").Select(x => new ToDo()
+            {
+            
+                ToDoId = x.ToDoId,
+                Name = x.Name,
+                Done = x.Done,
+                Category = x.Category != null ? new Category()
+                {
+                    CategoryId = x.Category.CategoryId,
+                    CatDesc = x.Category.CatDesc,
+                    CatName = x.Category.CatName,
+                } : null
+
+            }).ToListAsync();
+
+            return Ok(todos);
+
+        }
+
+        // GET: api/Resources/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDo>> GetToDo(int id)
+        {
+
+            if (_context.ToDos == null)
+            {
+                return NotFound();
+            }
+            var resource = await _context.ToDos.Where(x => x.ToDoId == id).Select(x => new ToDo()
+            { 
+                ToDoId = x.ToDoId,
+                Name = x.Name,
+                Done = x.Done,
+                CategoryId = x.CategoryId,
+                Category = x.Category != null ? new Category()
+                {
+                    CategoryId = x.Category.CategoryId,
+                    CatName = x.Category.CatName,
+                    CatDesc = x.Category.CatDesc
+                } : null
+            }).FirstOrDefaultAsync();
+
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(resource);
+        }
+
+        // PUT: api/Resources/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDo(int id, ToDo toDo)
+        {
+            if (id != toDo.ToDoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Resources
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ToDo>> PostToDo(ToDo toDo)
+        {
+            if (_context.ToDos == null)
+            {
+                return Problem("Entity set 'ResourcesContext.Resources' is null");
+            }
+            
+            ToDo newtoDo = new ToDo()
+            {
+                Name = toDo.Name,
+                Done = toDo.Done,
+                CategoryId = toDo.CategoryId
+            };
+
+            _context.ToDos.Add(newtoDo);
+            await _context.SaveChangesAsync();
+
+            return Ok(toDo);
+        }
+
+        // DELETE: api/Resources/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDo(int id)
+        {
+            var todo = await _context.ToDos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ToDoExists(int id)
+        {
+            return _context.ToDos.Any(e => e.ToDoId == id);
+        }
+    }
+}
